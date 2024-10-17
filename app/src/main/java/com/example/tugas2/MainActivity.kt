4d814b4ad96511c6b122b5dcb59e1d3d1a7cd906
DeniArif <deniarifwibowo82@gmail.com>
2024-10-18 04:06:18 +0700

    Membuat code untuk menambahkan gambar dan mengubah ukuran gambar

--- app/src/main/java/com/example/tugas2/MainActivity.kt
+++ app/src/main/java/com/example/tugas2/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,6 +56,11 @@ fun Biodata(modifier: Modifier = Modifier){
         Spacer(modifier = Modifier.padding(5.dp))
         Text("Ini adalah halaman login", fontSize = 30.sp)
         Spacer(modifier = Modifier.padding(5.dp))
+        Image(
+            painter = painterResource(id = R.drawable.UMY),
+            contentDescription = "",
+            modifier = Modifier.clip(CircleShape).size(200.dp)
+        )
 
     }
 }
