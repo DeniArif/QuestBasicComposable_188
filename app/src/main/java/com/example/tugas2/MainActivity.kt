2cab2bb2faf3de79210fd815410e267635210728
DeniArif <deniarifwibowo82@gmail.com>
2024-10-19 07:27:35 +0700

    Mengubah fontStyle pada teks

--- app/src/main/java/com/example/tugas2/MainActivity.kt
+++ app/src/main/java/com/example/tugas2/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,7 +64,9 @@ fun Biodata(modifier: Modifier = Modifier){
         )
         Spacer(modifier = Modifier.padding(10.dp))
         Text( "Nama",
-            fontSize = 10.sp,)
+            fontSize = 20.sp,
+            fontStyle = FontStyle.Italic
+        )
 
 
 
